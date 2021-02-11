c132a04137e3df1f179811fb3d4ecef1f18a0fc2
DannyVanIets <dannyvanbokhorst@live.nl>
2021-02-11 10:18:24 +0100

    Alle voorbeelden van lecture 1

---
+++ lecture 1/CG - Lecture 1 - 2 - Cube rotating/scene.js
@@ -0,0 +1,39 @@
+// Create scene
+var scene = new THREE.Scene();
+
+// Create camera
+var camera = new THREE.PerspectiveCamera(
+    75,     // fov - Camera frustum vertical field of view
+    window.innerWidth / window.innerHeight, // aspect - Camera frustum aspect ratio
+    0.1,   // near - Camera frustum near plane
+    1000); // far - Camera frustum far plane
+
+// Create renderer
+var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create geometry
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshNormalMaterial();
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Move camera from center
+camera.position.x = 2;  // Move right from center of scene
+camera.position.y = 1;  // Move up from center of scene
+camera.position.z = 5;  // Move camera away from center of scene
+
+var clock = new THREE.Clock();
+
+var render = function() {
+    requestAnimationFrame(render);
+
+    var delta = clock.getDelta();
+    cube.rotation.x += 3.2 * delta;
+    cube.rotation.y += 1.2 * delta;
+
+    renderer.render(scene, camera);
+}
+
+render();
---
+++ lecture 1/CG - Lecture 1 - 4 - Cube with texture/scene.js
@@ -0,0 +1,46 @@
+// Create scene
+var scene = new THREE.Scene();
+
+// Create camera
+var camera = new THREE.PerspectiveCamera(
+    75,     // fov - Camera frustum vertical field of view
+    window.innerWidth / window.innerHeight, // aspect - Camera frustum aspect ratio
+    0.1,   // near - Camera frustum near plane
+    1000); // far - Camera frustum far plane
+
+// Create renderer
+var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create geometry
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('Yellobrk.bmp')});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Define light
+var light = new THREE.DirectionalLight(0xdddddd, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+// Move camera from center
+camera.position.x = 2;  // Move right from center of scene
+camera.position.y = 1;  // Move up from center of scene
+camera.position.z = 5;  // Move camera away from center of scene
+
+// Import camera control and rotation library
+controls = new THREE.OrbitControls(camera); 
+controls.autoRotate = true;
+controls.autoRotateSpeed = 2;
+controls.noKeys = true;
+
+var render = function() {
+    requestAnimationFrame(render);
+
+    controls.update();
+    
+    renderer.render(scene, camera);
+}
+
+render();
